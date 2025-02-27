b3d1850719a0afd6e44eb2485078aca2bb134f65
dhurtig <112945014+dhurtig1595@users.noreply.github.com>
2025-02-27 14:50:19 +0100

    first commit

---
+++ dataparser.py
@@ -0,0 +1,49 @@
+from scipy.io import wavfile
+import wave, os, glob
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+
+
+def audio_parser(filepath_clean, filepath_dist, buffer_size):
+    # This function reads audio files from folders, discards one of the channels,
+    # and batches them into batches of 512.
+    clean_buffers = []
+    dist_buffers = []
+    print("we're in..")
+    for filename in glob.glob(os.path.join(filepath_clean, '*.wav')):
+        name = filename.split("/")[2].split("_")
+        # Use the name to get the parameter values from the mixing table
+        trim, high, mid, skew, low = int(name[1][1:]), int(name[2][1:]), int(name[3][1:]), int(name[4][4:]), int(name[5][1:].split(".")[0]), 
+        id = name[0][1]
+        settings = "_".join(name[1:])
+        _, data_clean = wavfile.read(filename)
+        data_clean = data_clean[:, 0]        # Take only first column, they are identical
+        _, data_dist = wavfile.read(filepath_dist + "/D" + id + "_" + settings)
+        data_dist = data_dist[:, 0]         # We take first row, idk why let's try it
+
+        print("shape data clean: ", np.shape(data_clean))
+        print("shape data dist: ", np.shape(data_dist))
+
+        # Fix the batches of clean data
+        clean_chunks = [data_clean[i:i + buffer_size] for i in range(0, len(data_clean), buffer_size)]
+        # We want to make rows of size 512 for each parameter, so we end up with a matrix of size 6x512, first row is audio, rest are parameters
+        row = np.ones(512)
+        parameters = [row*trim, row*high, row*mid, row*skew, row*low]
+        clean_parameter_chunks = []
+        for chunk in clean_chunks[:-1]:
+            chunk = np.vstack([chunk, parameters])
+            clean_parameter_chunks.append(chunk.T)
+        clean_chunks = np.array(clean_parameter_chunks)
+
+        dist_chunks = [data_dist[i:i + buffer_size] for i in range(0, len(data_dist), buffer_size)]
+
+        print("shape clean buffer chunks: ", np.shape(clean_chunks))
+        print("shape dist buffer chunks: ", np.shape(dist_chunks[:-1]))
+      
+    # Return everything except the last chunk of dist_chunks which may not be of size 512
+    return clean_chunks, np.array(dist_chunks[:-1])
+
+
+#data, targets = audio_parser("Training Data/C", "Training Data/D", 512)
+
---
+++ train.py
@@ -0,0 +1,79 @@
+import torch
+from torch.utils.data import Dataset
+import numpy as np
+import torch.nn as nn
+import matplotlib.pyplot as plt
+import json
+from json import JSONEncoder
+from dataparser import audio_parser
+import torch.optim as optim
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader, TensorDataset
+from tqdm import tqdm
+
+class EncodeTensor(JSONEncoder,Dataset):
+    def default(self, obj):
+        if isinstance(obj, torch.Tensor):
+            return obj.cpu().detach().numpy().tolist()
+        return super(json.NpEncoder, self).default(obj)
+
+class LSTM_Model(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        # LSTM expects input of shape (batch_size, sequence_length, input_size)
+        self.lstm = nn.LSTM(input_size=6, hidden_size=50, num_layers=1, batch_first=True)
+        self.linear = nn.Linear(50, 512)  # Map from hidden size to output size (512)
+    
+    def forward(self, x):
+        # x shape: (batch_size, sequence_length=512, input_size=6)
+        x, _ = self.lstm(x)  # Output shape: (batch_size, 512, hidden_size=50)
+        x = x[:, -1, :]       # Take the last time step (batch_size, 50)
+        x = self.linear(x)    # Output shape: (batch_size, 512)
+        return x
+
+def train(x, y, n_epochs):
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+        x_train = torch.tensor(x_train, dtype=torch.float32)
+        y_train = torch.tensor(y_train, dtype=torch.float32)
+        x_test = torch.tensor(x_test, dtype=torch.float32)
+        y_test = torch.tensor(y_test, dtype=torch.float32)
+
+        # Set up the model
+        model = LSTM_Model()
+        optimizer = optim.Adam(model.parameters())
+        loss_fn = nn.MSELoss()
+        
+        # Load the training data
+        train_dataset = TensorDataset(x_train, y_train)
+        loader = DataLoader(train_dataset, shuffle=True, batch_size=8)
+
+        print("training started..")
+        for epoch in range(n_epochs):
+            model.train()
+            for X_batch, y_batch in tqdm(loader, desc=f"Epoch {epoch+1}/{n_epochs}", leave=False):
+                y_pred = model(X_batch)
+                loss = loss_fn(y_pred, y_batch)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+            # Validation
+            if epoch % 100 != 0:
+                continue
+            model.eval()
+            with torch.no_grad():
+                y_pred = model(x_train)
+                train_rmse = np.sqrt(loss_fn(y_pred, y_train))
+                y_pred = model(x_test)
+                test_rmse = np.sqrt(loss_fn(y_pred, y_test))
+            print("Epoch %d: train RMSE %.4f, test RMSE %.4f" % (epoch, train_rmse, test_rmse))
+            return model
+    
+
+
+
+data, targets = audio_parser("Training Data/C", "Training Data/D", 512)
+trained_model = train(data, targets, 2000)
+
+
+with open('models/gru_torch.json', 'w') as json_file:
+    json.dump(trained_model.state_dict(), json_file,cls=EncodeTensor)
